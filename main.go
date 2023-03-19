5b697588774cce1cd2189945d4fd887f89abeb92
GameDog9988 <alex@grattan.me>
2023-03-19 15:32:59 -0400

    feat: add json tags for marshaled values

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -23,15 +24,15 @@ type server struct {
 }
 
 type Kitchen struct {
-	ID          int8
-	CreatedAt   string
-	UpdatedAt   string
-	Name        string
-	Logo        sql.NullString
-	Description sql.NullString
-	WebsiteLink sql.NullString
-	ParentID    sql.NullInt16
-	Type        string
+	ID          int8           `json:"id"`
+	CreatedAt   time.Time      `json:"created_at"`
+	UpdatedAt   time.Time      `json:"updated_at"`
+	Name        string         `json:"name"`
+	Logo        sql.NullString `json:"logo"`
+	Description sql.NullString `json:"description"`
+	WebsiteLink sql.NullString `json:"website_link"`
+	ParentID    sql.NullInt16  `json:"parent_id"`
+	Type        string         `json:"type"`
 }
 
 func (s *server) hello(w http.ResponseWriter, r *http.Request) {
